2dc4918fde3d7a46802a2a3f10cf1f0a666705d6
Rixomg <150179034+Rixomg@users.noreply.github.com>
2023-11-22 11:36:29 +0100

    Create python.py
    GP Bahrein

---
+++ python.py
@@ -0,0 +1,2 @@
+#leiri
+print('Alonso cruza la línea de meta y después de 11 años de sufrimiento, el samurái saca a relucir su espada, el águila ha vuelto, los pajaritos han salido de la jaula, Fernando se proclama ganador del Gran Premio de Bahréin con el aston martin de 2024')
